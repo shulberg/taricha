6213669814e6721af98a927fabf9fa4c07e66672
Scott Hulberg <scott@openpolliwog.org>
2014-05-31 14:41:47 -0700

    inital commit

---
+++ taricha.h
@@ -0,0 +1,24 @@
+#ifndef _taricha_H
+#define _taricha_H
+
+#include <stdint.h>
+
+struct taricha_state
+{
+	uint8_t buffer[64];
+	size_t buffer_free;
+	uint64_t s[8];
+};
+
+struct taricha_state taricha_init();
+
+void taricha_append(const uint8_t *in, size_t length,
+		struct taricha_state *s);
+
+void taricha_finalize(uint8_t *out, size_t length,
+		struct taricha_state *s);
+
+void taricha_full(uint8_t *in, uint8_t *out,
+		size_t in_length, size_t out_length);
+
+#endif
